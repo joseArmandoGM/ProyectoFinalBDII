4d77e2ae8157003e6ae64e7751e8392b9c9b0989
Jose Armando Garcia Marin <147743780+joseArmandoGM@users.noreply.github.com>
2024-05-14 11:26:22 -0600

    Add files via upload

---
+++ src/main/java/com/sgecj/eventos/service/CategoriaService.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sgecj.eventos.service;
+
+import com.sgecj.eventos.domain.Categoria;
+import com.sgecj.eventos.repository.CategoriaRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author huevo
+ */
+@Service
+public class CategoriaService {
+    private final CategoriaRepository categoriaRepository;
+    
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+    
+    public List<Categoria> buscarTodos() {
+        return categoriaRepository.findAll();
+    }
+    
+    public Categoria guardar(Categoria categoria) {
+        return categoriaRepository.save(categoria);
+    }
+}
