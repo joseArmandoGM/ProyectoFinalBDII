21457c48b245024d7aabd6cb2002dcedcb405f62
Jose Armando Garcia Marin <147743780+joseArmandoGM@users.noreply.github.com>
2024-05-17 13:02:06 -0600

    Add files via upload
    Cambios extra

--- src/main/java/com/sgecj/eventos/repository/ActividadRepository.java
+++ src/main/java/com/sgecj/eventos/repository/ActividadRepository.java
@@ -5,7 +5,9 @@
 package com.sgecj.eventos.repository;
 
 import com.sgecj.eventos.domain.Actividad;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
@@ -13,4 +15,42 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface ActividadRepository extends JpaRepository <Actividad, Integer>{
     
+    /**
+     * Retorna la lista de actividades por nombre
+     * @return
+     */
+    @Query("Select a from Actividad a order by a.nombre")
+    List<Actividad> buscarTodos();
+    
+    /**
+     * Retorna la lista de actividades segun el evento al que pertenece
+     * @param eventoId
+     * @return 
+     */
+    @Query("from Actividad a where a.evento.id = ?1 order by a.nombre")
+    List<Actividad> buscarPorEvento(int eventoId);
+    
+    /**
+     * Retorna la lista de actividades segun el area tematica a la que pertenece
+     * @param areaTematicaId
+     * @return 
+     */
+    @Query("from Actividad a where a.areaTematica.id = ?1 order by a.nombre")
+    List<Actividad> buscarPorAreaTematica(int areaTematicaId);
+    
+    /**
+     * Retorna la lista de actividades segun la categoria a la que pertenece
+     * @param categoriaId
+     * @return 
+     */
+    @Query("from Actividad a where a.categoria.id = ?1 order by a.nombre")
+    List<Actividad> buscarPorCategoria(int categoriaId);
+    
+    /**
+     * Busca las actividades por su nombre
+     * @param consulta
+     * @return 
+     */
+    @Query("from Actividad a where a.nombre like %?1%")
+    List<Actividad> buscar(String consulta);
 }
---
+++ src/main/java/com/sgecj/eventos/service/AreaTematicaService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sgecj.eventos.service;
+
+import com.sgecj.eventos.domain.AreaTematica;
+import com.sgecj.eventos.repository.AreaTematicaRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author huevo
+ */
+@Service
+public class AreaTematicaService {
+    private final AreaTematicaRepository areaTematicaRepository;
+    
+    public AreaTematicaService(AreaTematicaRepository areaTematicaRepository) {
+        this.areaTematicaRepository = areaTematicaRepository;
+    }
+    
+    public List<AreaTematica> buscarTodos() {
+        return areaTematicaRepository.findAll();
+    }
+}
